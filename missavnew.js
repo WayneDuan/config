2c64e7df40e02d5c6141a082b23804ad3877daef
duany0816 <duany0816@gmail.com>
2026-04-02 21:37:14 +0800

    更新js

--- missavnew.js
+++ missavnew.js
@@ -1,4 +1,4 @@
-const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)';
+const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1';
 const SITE = 'https://missav.ai';
 const cheerio = createCheerio(); // 假设你的新环境依然支持 createCheerio 和 $fetch
 
